b5009b3baa71aaffcab11befaed68661426fdfa0
shokhruzisroilov <shokhruzisroilov@gmail.com>
2024-04-23 10:27:43 +0500

    The Desktop min 1024px responsave of the Header is done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,14 +6,14 @@ import { styles } from '../../utils/styles'
 const Header: FC = () => {
 	return (
 		<div className='w-full h-20 bg-white flex items-center justify-between px-5'>
-			<div className='pl-10'>
-				<h2 className='text-textYellow font-monda font-[900] text-[32px] leading-[20px] tracking-normal shadow-sm'>
+			<div className='pl-10 max-2xl:pl-0'>
+				<h2 className='text-textYellow font-monda font-[900] text-[32px] max-3xl:text-2xl leading-[20px] max-3xl:leading-[14px] tracking-normal shadow-sm cursor-pointer'>
 					&nbsp;Ish <br />
 					top
 				</h2>
 			</div>
 			<nav>
-				<ul className='flex items-center gap-10'>
+				<ul className='flex items-center gap-10 max-3xl:gap-5'>
 					<li>
 						<div className={`${styles.headerActiveClass}`}>Asosiy</div>
 					</li>
@@ -27,9 +27,9 @@ const Header: FC = () => {
 						<div className={`${styles.headerClass}`}>Ishchi qidiryapman</div>
 					</li>
 					<li>
-						<div className='w-full h-10 bg-white border-searchInputBorder border-[1px] border-solid rounded-3xl flex items-center'>
+						<div className='w-full h-10 bg-white border-searchInputBorder border-[1px] border-solid rounded-3xl flex items-center max-2xl:hidden'>
 							<div className='flex items-center px-3 gap-3'>
-								<SearchOutlined className='text-primary text-2xl cursor-pointer' />
+								<SearchOutlined className='text-primary text-2xl max-3xl:text-xl cursor-pointer' />
 								<input
 									type='text'
 									placeholder='Qidirish...'
@@ -39,19 +39,22 @@ const Header: FC = () => {
 							<div className='w-[1px] h-7 bg-searchInputBorder'></div>
 							<div className='flex items-center gap-5 px-5'>
 								<div className={`${styles.textStyle}`}>Filtr</div>
-								<MdOutlineTune className='text-primary text-2xl cursor-pointer' />
+								<MdOutlineTune className='text-primary text-2xl max-3xl:text-xl cursor-pointer' />
 							</div>
 						</div>
+						<SearchOutlined className='text-primary text-2xl max-3xl:text-xl cursor-pointer 2xl:hidden' />
 					</li>
 				</ul>
 			</nav>
 			<div className='flex items-center gap-5'>
-				<button className={`${styles.textStyle} pr-5`}>Tizimga kirish</button>
-				<button className='px-8 h-[60px] bg-primary flex justify-center items-center rounded-[30px] text-white font-mako font-normal text-[20px] leading-[33px] tracking-normal'>
+				<button className={`${styles.textStyle} pr-5 max-3xl:pr-0`}>
+					Tizimga kirish
+				</button>
+				<button className='px-8 h-[60px] max-3xl:h-[50px] bg-primary flex justify-center items-center rounded-[30px] text-white font-mako font-normal text-[20px] max-3xl:text-base leading-[33px] tracking-normal'>
 					Ro’yxatdan o’tish
 				</button>
 				<div>
-					<GlobalOutlined className='text-primary text-2xl cursor-pointer' />
+					<GlobalOutlined className='text-primary text-2xl max-3xl:text-xl cursor-pointer' />
 				</div>
 			</div>
 		</div>
--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,8 +1,8 @@
 export const styles = {
 	headerClass:
-		'text-[#606060] font-mako text-[25px] font-normal leading-[33px] tracking-normal',
+		'text-[#606060] font-mako text-[25px] max-3xl:text-base font-normal leading-[33px] tracking-normal cursor-pointer',
 	headerActiveClass:
-		'px-8 h-10 bg-primary flex justify-center items-center rounded-3xl text-white font-mako font-normal text-[25px] leading-[33px] tracking-normal',
+		'px-8 h-10 bg-primary flex justify-center items-center rounded-3xl text-white font-mako font-normal text-[25px] max-3xl:text-base leading-[33px] tracking-normal cursor-pointer',
 	textStyle:
-		'text-primary font-monda text-[20px] font-normal leading-[33px] tracking-normal',
+		'text-primary font-monda text-[20px] max-3xl:text-base font-normal leading-[33px] tracking-normal',
 }
